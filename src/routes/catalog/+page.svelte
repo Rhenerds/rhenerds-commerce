2f1bb593619e009120e1498ac852e6a7246e987a
Kraafter <110327339+Kraafter@users.noreply.github.com>
2025-07-18 02:04:58 +0700

    Product view using blog rendering

---
+++ src/routes/catalog/+page.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import * as config from '$lib/config'
+
+	let { data } = $props()
+</script>
+
+<svelte:head>
+	<title>{config.title}</title>
+</svelte:head>
+
+<h1>Catalog</h1>
+<div class="ProdView">
+    {#each data.posts as post}
+        <a href="/catalog/{post.slug}" class="ProdItem">
+            <div class="ProdImg">
+            </div>
+            <div class="ProdInfo">
+            <p class="ProdText">{post.title}</p>
+            <p class="ProdText" style="font-weight: 420;">{post.price}</p>
+            {#if post.linkstate === 'PO'}
+                <p class="ProdText">Preorder Open</p>
+            {/if}
+            {#if post.linkstate === 'OTS'}
+                <p class="ProdText">On The Spot</p>
+            {/if}
+            {#if post.linkstate === 'SO'}
+                <p class="ProdText">Sold Out</p>
+            {/if}
+            {#if post.linkstate === 'U'}
+                <p class="ProdText">Unavailable</p>
+            {/if}
+            </div>
+        </a>
+    {/each}
+</div>
+
+<style>
+    h1 {
+        color: #000;
+        font-family: "Roboto Flex";
+        font-size: 64px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 90%; /* 57.6px */
+        letter-spacing: -1.92px;
+        margin-bottom: 90px;
+    }
+
+    .ProdView {
+        width: 100%;
+        height: fit-content;
+        display: flex;
+        gap: 20px;
+        flex-wrap: wrap;
+        justify-content: left;
+    }
+    .ProdItem {
+         border-radius: 10px;
+         background-color: #E4E4E4;
+         width: 170px;
+         height: 280px;
+    }
+    .ProdImg {
+        border-radius: 10px;
+        background-color: aqua;
+        width: 170px;
+        height: 170px;
+    }
+
+    .ProdInfo {
+        width: 100%;
+        height: 110px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .ProdText {
+        font-family: 'Roboto Flex';
+        margin: 5px;
+        width: auto;
+        text-align: center;
+        color: #000;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: 90%; /* 18px */
+        letter-spacing: -0.6px;
+    }
+</style>
