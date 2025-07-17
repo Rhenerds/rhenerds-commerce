722224108773c71dc7e414021b04487e18e80f62
Kraafter <110327339+Kraafter@users.noreply.github.com>
2025-07-18 02:02:55 +0700

    Added quick configuration grabbing

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'The Nerds'
+export const description = 'Catalog site'
+export const url = dev ? 'http://localhost:5173/' : 'https://catalog.nerds.my.id/'
