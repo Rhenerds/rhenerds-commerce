a5c545057d1b55d43afdb9d1fefa9b4dfcdbe7cf
Kraafter <110327339+Kraafter@users.noreply.github.com>
2025-07-18 02:02:44 +0700

    set up mdsvex for product rendering

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,17 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const mdsvexOptions = {
+	extensions: ['.md'],
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex()],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: { adapter: adapter() },
-	extensions: ['.svelte', '.svx']
+	extensions: ['.svelte', '.svx', '.md']
 };
 
 export default config;
