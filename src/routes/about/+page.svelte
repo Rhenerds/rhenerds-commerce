10a4000bc0d1fa8eaf6477a34c8842937b7217ae
Kraafter <110327339+Kraafter@users.noreply.github.com>
2025-07-19 13:28:16 +0700

    Added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,65 @@
+<article>
+    <h1>Who are we?</h1>
+    <p>We are a small friend group of mostly Indonesian furries with various hobbies, interests, and quirks. We actually began from a group of furries within a coding competition server but grew quickly with friends made along they way!</p>
+    <h1>What is this website?</h1>
+    <p>This website is purposed as a catalog information and preordering website to aid us in selling goods and merchandise at Comifuro 21, being held later this year. This site will also be repurposed after CF21 if such demands are present.</p>
+    <h1>How do I preorder?</h1>
+    <p>Preorders may not be available for every product, but if available you may click on the preorder button and you will be redirected to Doku for payment and invoicing.</p>
+    <h1>When are stocks updated?</h1>
+    <p>Stocks may not be updated as frequent but we will try to inform of outages as soon as possible.</p>
+</article>
+
+<div class="links">
+    <a class="link" href="https://instagram.com/rhe_nerds/" target="_blank">Instagram</a>
+    <a class="link" href="https://github.com/Rhenerds" target="_blank">Github</a>
+</div>
+
+<style>
+    h1 {
+        color: #000;
+        font-family: "Roboto Flex";
+        font-size: 40px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 90%; /* 36px */
+        letter-spacing: -1.2px;
+        margin-bottom: 10px;
+    }
+
+    p {
+        color: #000;
+        font-family: "Roboto Flex";
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: 98%; /* 18px */
+        letter-spacing: -0.6px;
+        margin-bottom: 20px;
+    }
+
+    .links {
+        display: flex;
+        flex-direction: row;
+        gap: 40px;
+        justify-content: center;
+        width: 100%;
+        height: fit-content;
+        margin-top: 20px;
+    }
+
+    .link {
+        color: #002b88;
+        text-align: center;
+        font-family: "Roboto Flex";
+        font-size: 30px;
+        font-style: normal;
+        font-weight: 300;
+        line-height: 90%; /* 18px */
+        letter-spacing: -0.6px;
+        width: 200px;
+    }
+
+    .link:hover {
+        text-decoration: underline 2px;
+    }
+</style>
