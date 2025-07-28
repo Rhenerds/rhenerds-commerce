46d9746f51dcbffadd601905c0649c5bee96d0f0
ivory willows <105597411+undrscre@users.noreply.github.com>
2025-07-28 18:53:39 +0800

    clean up redundant code

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,10 @@
 import type { Post } from '$lib/types'
 
+// iv - there's probably a better place to put this but for neow it's okay
+const fandoms = ['Zachz Winner', 'YonKaGor', 'Vocaloid UTAU', 'DELTARUNE', 'Fabulous Beast / YouShouYan', 'Forsaken', 'Public Transit', 'Beastars', 'Ena Dream BBQ', 'Arknights']
+
 export async function load({ fetch }) {
 	const response = await fetch('/api/productquery')
 	const posts: Post[] = await response.json()
-	return { posts }
+	return { posts, fandoms }
 }
