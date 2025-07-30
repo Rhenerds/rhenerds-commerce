04b51ac9e615174503818dd3efb20cb07387263d
Kraafter <110327339+Kraafter@users.noreply.github.com>
2025-07-31 01:35:51 +0700

    Enhance preordering notice contents and update to better reflect purchase flow

--- src/routes/legal/preordering/+page.svelte
+++ src/routes/legal/preordering/+page.svelte
@@ -3,7 +3,7 @@
 <p>With the approach of Comifuro 21 later November 15-16, we are opening preorders soon for your favorite merch!</p>
 <p>Preorder period has not been determined, check back later!</p>
 <h2>Preordering</h2>
-<p>Buyers will have to pick which items to preorder from listings that state preordering is open, each listing has a preorder button which redirects users to our Doku page.</p>
+<p>Purchasers will have to pick which items to preorder from listings that state preordering is open, purchasers would then add their desired products into their cart, and proceed with payment.</p>
 <h2>Payment</h2>
 <p>Payment and invoicing is fully done through Doku. Doku offers a wide range of payment options which include QRIS, bank, and open loop bank cards. After a successful purchase, an invoice will be sent to the purchasers prefered email. Purchasers are expected to keep their invoice for product pickup.</p>
 <h2>Pickup</h2>
@@ -12,6 +12,8 @@
 <p>Incase you are unable to pickup your purchased goods during the event, you may opt for an after event pickup by contacting us through the Whatsapp group. Options are meeting a member of the selling team at a predetermined location and time, or having it be shipped to your location through a courier service. Courier charges are to be paid by the customer. Unclaimed purchased goods will no longer be available for pickup after 31st of December 2025 12.00 PM GMT+7.</p>
 <h2>Refund Policy</h2>
 <p>Purchased goods are non refundable. However, a partial refund will be available for unclaimed purchases after they are unavailable for pickup.</p>
+
+<p>Last updated 31 July 2025</p>
 
 <style>
     .Link {
